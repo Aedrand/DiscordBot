f7f538cd715113e2ab3c4c247b7b612860a03bcd
Aedrand <riggs.andrew@gmail.com>
2016-12-27 10:36:06 -0500

    Add files via upload

---
+++ BotClass.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class BotClass
+{
+	public BotClass()
+	{
+	}
+}
---
+++ DiscordBot/BotClass.cs
@@ -0,0 +1,52 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+
+namespace DiscordBot
+{
+    class BotClass
+    {
+        DiscordClient client;
+
+
+        public BotClass()
+        {
+            
+
+            client = new DiscordClient(input =>
+            {
+                input.LogLevel = LogSeverity.Info;
+                input.LogHandler = Log;
+            });
+
+            client.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
+            });
+
+            var commands = client.GetService<CommandService>();
+
+            commands.CreateCommand("test").Do(async (e) =>
+            {
+                await e.Channel.SendMessage("Test");
+            });
+
+            commands.CreateCommand("play").Do(async (e) =>
+            {
+                await e.Channel.SendMessage("FUCK YOU");
+            });
+
+            client.ExecuteAndWait(async () =>
+            {
+                await client.Connect("MjMwMDcxNTM5OTE4MzA3MzM5.C0HHBA.ATque64ZipWfSAitA2FMMV0f7ao", TokenType.Bot);
+            });
+
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
